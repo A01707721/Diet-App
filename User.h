5c509bcb16e7fe2b75661dca030d0c1e137cec7d
A01707721 <150314726+A01707721@users.noreply.github.com>
2024-11-24 15:59:01 -0600

    Update User.h

--- User.h
+++ User.h
@@ -17,15 +17,21 @@ using namespace std;
 //Clase usuario con datos genericos
 class User{
     private:
-    //Atributos
+    //Atributos de la clase: nombre del usuario (name), edad (age), peso (weight), meta de calorias del dia (calory_goal)
     string name;
     int age;
     float weigth;
     float calory_goal;
     public:
-    //Constructor default
+    /**Constructor default 
+    *@param
+    *@return objeto User
+    */
     User():name(""), age(0), weigth(0), calory_goal(0){};
-    //Constructor 
+    /**Constructor donde se reciben las variables para los diferentes atributos
+    *@param string n: el nombre del usuario, int a: edad, float w: peso, float cg: meta de calorias
+    *@return objeto User
+    */
     User(string n,int a,float w,float cg): name(n), age(a), weigth(w), calory_goal(cg){};
     //Getters y Setters
     string get_name(){
@@ -56,16 +62,22 @@ class User{
 
 //Clase con informacion de los alimentos
 class Food{
-    //Atributos
+    //Atributos de la clase: nombre del alimento (name), tipo de comida (type), tamano de la porcion (size), cantidad de calorias (calories)
     private:
     string name;
     string type;
     float size;
     float calories;
     public:
-    //Constructor default
+    /**Constructor default 
+    *@param
+    *@return objeto Food
+    */
     Food():name(""),type(""),size(0),calories(0){};
-    //Constructor
+    /**Constructor donde se reciben las variables para los diferentes atributos
+    *@param string n: el nombre del alimento, string t: tipo de alimento, float s: tamano de la porcion
+    *@return objeto Food
+    */
     Food(string n,string t,float s):name(n),type(t),size(s),calories(0){};
     //Getters
     string get_name();
@@ -77,22 +89,42 @@ class Food{
     string display();
 };
 
+/**Getter nombre del alimento
+*@param 
+*@return string:nombre 
+*/
 string Food::get_name(){
     return name;
 }
 
+/**Getter primera letra del nombre del alimento
+*@param 
+*@return char:primer caracter
+*/
 char Food::get_char(){
     return name[0];
 }
 
+/**Getter tipo
+*@param 
+*@return string:tipo de alimento
+*/
 string Food::get_type(){
     return type;
 }
 
+/**Getter tamano
+*@param 
+*@return float:tamano de la porcion
+*/
 float Food::get_size(){
     return size;
 }
 
+/**Getter calorias
+*@param 
+*@return float:cantidad de calorias
+*/
 float Food::get_calories(){
     float cal=0;
     if(type=="Carbohydrate" || type=="Protein"){
@@ -106,6 +138,10 @@ float Food::get_calories(){
     
 }
 
+/**Muestra los datos del objeto en una cadena de texto
+*@param 
+*@return string:datos del objeto 
+*/
 string Food::display(){
     stringstream aux;
     aux << name << "\n";
